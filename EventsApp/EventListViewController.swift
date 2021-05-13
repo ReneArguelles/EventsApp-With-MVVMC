d32a91fb8128be2123c71d59657b62cf5e34f7d2
René Argüelles Iturriaga <ing.rene.arguelles@gmail.com>
2021-05-13 13:13:23 -0500

    Finished part 1

--- EventsApp/EventListViewController.swift
+++ EventsApp/EventListViewController.swift
@@ -14,8 +14,23 @@ class EventListViewController: UIViewController {
         let controller = storyboard.instantiateViewController(identifier: "EventListViewController") as! EventListViewController
         return controller
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
+    }
+    
+    private func setupViews() {
+        let plusImage = UIImage(systemName: "plus.circle.fill")
+        let barButtonItem = UIBarButtonItem(image: plusImage, style: .plain, target: self, action: #selector(tappedRightBarButton))
+        barButtonItem.tintColor = .primary
+        navigationItem.rightBarButtonItem = barButtonItem
+        navigationItem.title = "Events"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    @objc
+    private func tappedRightBarButton() {
+        print("tapped Right Bar Button")
     }
 }
---
+++ EventsApp/UIColor+Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  UIColor+Extensions.swift
+//  EventsApp
+//
+//  Created by Arguelles, Rene on 13/05/21.
+//
+
+import UIKit
+
+extension UIColor {
+    static let primary = UIColor(red: 0/255, green: 39/255, blue: 255/255, alpha: 0.6)
+}
